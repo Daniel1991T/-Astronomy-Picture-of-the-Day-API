20f18dcdd9b5923a3316db545c84015abf62734a
Daniel1991T <64424269+Daniel1991T@users.noreply.github.com>
2020-11-04 19:55:19 +0200

    improve UI design

--- app/src/main/java/com/compani/ilai/astronomydaypicturenasaapi/data/NasaPhoto.kt
+++ app/src/main/java/com/compani/ilai/astronomydaypicturenasaapi/data/NasaPhoto.kt
@@ -4,10 +4,10 @@ import com.squareup.moshi.JsonClass
 
 @JsonClass(generateAdapter = true)
 data class NasaPhoto(
-    val copyright: String,
-    val date: String,
-    val explanation: String,
-    val hdurl: String,
-    val title: String,
-    val url: String
+    val copyright: String?,
+    val date: String?,
+    val explanation: String?,
+    val hdurl: String?,
+    val title: String?,
+    val url: String?
 )
